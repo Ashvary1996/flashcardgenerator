c6900b151395ae7f00c50f0b0d2934c82826abc9
Ashvary Gidian <ashvarygidian1996@gmail.com>
2023-07-25 23:28:51 +0530

    library/packages added

--- src/components/testingfile.js
+++ src/components/testingfile.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { FaBeer } from "react-icons/fa";
 
-export default function testingfile() {
+export default function Testingfile() {
   return (
     <div>
-      <h1>Testing</h1>
+      <h1 className="font-extrabold underline bg-teal-500 text-white">
+        Testing Successfull
+      </h1>
+      <h3>
+        {" "}
+        Lets go for a <FaBeer />
+      </h3>
     </div>
-  )
+  );
 }
