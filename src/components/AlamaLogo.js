5f695cd9ad31d51496047a43eab75a79564bf5b1
Ashvary Gidian <ashvarygidian1996@gmail.com>
2023-07-28 15:36:24 +0530

    logo problem fixed .

---
+++ src/components/AlamaLogo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import almaBetterLogo from "../assets/almaBetterLogo.png";
+
+function AlamLogo() {
+  return (
+    <div className=" h-8 py-8 bg-[#ffffff] border-b-4 border-[#f0edeb]">
+      <img className="w-32 -my-4 mx-8" src={almaBetterLogo} alt="" />
+    </div>
+  );
+}
+
+export default AlamLogo;
